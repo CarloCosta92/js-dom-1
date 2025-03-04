0f878365b2aba5aac398c9fff301441ea5530b4a
Carlo Costa <carlo.costa92@gmail.com>
2025-03-04 15:46:00 +0100

    feat: bonus solved

--- js/app.js
+++ js/app.js
@@ -16,9 +16,26 @@
     // Per fare questo bonus potremmo aver bisogno di del metodo string.includes() 
 
 
-    // se è accesa scrivi spegni se spenta scrivi accesa(usare un if per le condizioni)
+    // se è accesa scrivi spegni se spenta scrivi accendi(usare un if per le condizioni)
     
     // metodo includes per vedere se accesa o spenta ==> includes(searchString) ==> This method lets you determine whether or not a string includes another string.
 
 
     // funzione
+
+    function accendi(){
+        const lampadina = document.getElementById("lampadina");
+        const bottone = document.getElementById("bottone");
+
+        if (lampadina.src.includes("white")) {
+            
+            lampadina.src = "./img/yellow_lamp.png";
+            bottone.innerHTML = "Spegni"; 
+
+        } else {
+                        
+            lampadina.src = "./img/white_lamp.png"; 
+            bottone.innerHTML = "Accendi"; 
+    }
+}
+    
